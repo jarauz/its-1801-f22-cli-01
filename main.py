31469f0a6f739eb90d3d88eaaff21f076a1196df
Julio Arauz <jarauz@alumni.pitt.edu>
2022-11-29 15:37:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# Practice commands for Linux shell
+
+# Shell is a program that can be used to run commands
+# usually called system commands
+
+# ~/its-1801-f22-cli-01$ is called a prompt
+
+# ~ represents the home directory
+# / is a separator for directories and or files
+# its-1801-f22-cli-01 is another directory
+
+# ls is for listing files and directories in the current directory
+
+# ls -l is for long list format
+# the first letter indicates if it is a directory (d) or a file (-)
+
+# ls -al is for long format and showing hidden files
+
+# hidden files, have names that start with a dot (.)
+
+# cd is for changing directories (notice that the prompt also reflects the change)
+
+
+# To go up one directory:
+# cd ..
+
+# Shells usually have autocomplete using the TAB key
+
+# pwd (present working directory) and shows the full path to the directory you are currently in
+
+# cat command (cat stands for catalog)
+# To show the contents of a text file (file that has characters that are printable on the screen)
