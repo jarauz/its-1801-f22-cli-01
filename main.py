371b250ed3658381608fb2fa88e2a9e9e210044c
Julio Arauz <jarauz@alumni.pitt.edu>
2022-12-01 15:15:26 +0000

    Added commands mv, rm, rmdir, cp, touch

--- main.py
+++ main.py
@@ -9,6 +9,33 @@
 # / is a separator for directories and or files
 # its-1801-f22-cli-01 is another directory
 
+# When you run ls -al
+# You'll see hidden files (name starts with .)
+# Also . and ..
+# . stands for the current directory
+# .. stands for the parent directory (of the current directory)
+
+# touch can be used to create an empty file
+# example touch file1
+
+# To rename a file you use the mv command
+# Example mv file1 file1.txt
+
+# To create a directory you use the mkdir command
+# Example mkdir testDir
+
+# To move a file you use the mv command
+# Example mv file1.txt ./testDir
+
+# To copy a file you use the cp command
+# Example cp file1.txt ~ (to compy file1.txt to your home directory)
+
+# To remove a file use the rm command
+# Example rm file1.txt
+
+# To remove a directory you use the rmdir command
+# Example rmdir testDir
+
 # ls is for listing files and directories in the current directory
 
 # ls -l is for long list format
@@ -34,3 +61,7 @@
 # more command (catalogs a text file page by page ). To advance use the space key, to quit press q (or press CTRL-C)
 
 # clear command clears the screen
+
+# https://www.digitalocean.com/community/tutorials/linux-commands
+
+# https://learn.microsoft.com/en-us/training/modules/introduction-to-powershell/
